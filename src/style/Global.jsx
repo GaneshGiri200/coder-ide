c6e1572e84cad5cd50748adc19f40d8a6f03f422
thecodertech <giriganesh775@gmail.com>
2022-11-21 00:12:56 +0530

    Changed Font for Website

--- src/style/Global.jsx
+++ src/style/Global.jsx
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
-        font-family: 'Roboto Flex', sans-serif;
+        font-family: 'Roboto Slab', sans-serif;
     }
 `;
 
